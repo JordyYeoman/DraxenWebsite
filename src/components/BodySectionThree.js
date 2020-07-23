930af5f0de68ccd4e9d3c3282ff1522e6a18ad5d
Jordy Yeoman <Jordyyeoman@gmail.com>
2020-07-23 13:13:27 +0800

    completed Build Section on bottom of homepage

--- src/components/BodySectionThree.js
+++ src/components/BodySectionThree.js
@@ -8,6 +8,15 @@ import EdgeofNight from "../assets/img/EdgeofNight-Dravenbuilds.png";
 import Sanguine from "../assets/img/SanguineBlade-DravenBuilds.png";
 import Dblade from "../assets/img/doransblade-dravenbuilds.png";
 import Healthpot from "../assets/img/healthpotion-dravenbuilds.png";
+import Bloodthirster from "../assets/img/bloodthirster.png";
+import infedge from "../assets/img/infinityedge.png";
+import rfc from "../assets/img/rapidfirecannon.png";
+import zerkers from "../assets/img/berserkersgreaves.png";
+import betaboots from "../assets/img/ninjatabi.png";
+import deathsdance from "../assets/img/deathsdance.png";
+import guardianangel from "../assets/img/guardianangel.png";
+import essencereaver from "../assets/img/essencereaver.png";
+import youmuus from "../assets/img/yomuus.png";
 
 const BodySectionThree = () => {
   return (
@@ -54,6 +63,13 @@ const BodySectionThree = () => {
                       </div>
                       <div className="Draven-items">
                         <img
+                          src={zerkers}
+                          alt="Berzerker Boots Draven Crit Build"
+                        />
+                        Berserker's Greaves
+                      </div>
+                      <div className="Draven-items">
+                        <img
                           src={Manamune}
                           alt="Manamune-Draven Lethality Build"
                         />
@@ -73,13 +89,128 @@ const BodySectionThree = () => {
                         />
                         Edge of Night
                       </div>
+                      <div className="Draven-items">
+                        <img
+                          src={youmuus}
+                          alt="Youmuu's Ghostblade Draven Lethality Build"
+                        />
+                        Youmuu's Ghostblade
+                      </div>
                     </div>
                   </div>
                 </li>
-                <li className="draven-build-paths">Crit</li>
-                <li className="draven-build-paths">Hybrid</li>
-                <li className="draven-build-paths">Tank</li>
-                <li className="draven-build-paths">Survivor</li>
+                <li className="draven-build-paths">
+                  <div className="Draven-item-build lethality-build-path">
+                    <h3>Crit</h3>
+                    <div className="Draven-items-container">
+                      <div className="Draven-items">
+                        <img
+                          src={Dblade}
+                          alt="Doran's Blade-Draven Lethality Build"
+                        />
+                        Doran's Blade
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={Healthpot}
+                          alt="Health potion-Draven Lethality Build"
+                        />
+                        Health Potion
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={zerkers}
+                          alt="Berzerker Boots Draven Crit Build"
+                        />
+                        Berserker's Greaves
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={Bloodthirster}
+                          alt="Blood Thirster Draven Crit Build"
+                        />
+                        Blood thirster
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={infedge}
+                          alt="Infinity Edge Draven Crit Build"
+                        />
+                        Infinity Edge
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={rfc}
+                          alt="Rapid Fire Cannon Draven Crit Build"
+                        />
+                        Rapid Fire Cannon
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={essencereaver}
+                          alt="Essence Reaver Draven Crit Build"
+                        />
+                        Essence Reaver
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li className="draven-build-paths">
+                  <div className="Draven-item-build lethality-build-path">
+                    <h3>Survivor</h3>
+                    <div className="Draven-items-container">
+                      <div className="Draven-items">
+                        <img
+                          src={Dblade}
+                          alt="Doran's Blade-Draven Lethality Build"
+                        />
+                        Doran's Blade
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={Healthpot}
+                          alt="Health potion-Draven Survivor Build"
+                        />
+                        Health Potion
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={betaboots}
+                          alt="Berzerker Boots Draven Survivor Build"
+                        />
+                        Beta Boots (Ninja Tabi's)
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={Bloodthirster}
+                          alt="Blood Thirster Draven Survivor Build"
+                        />
+                        Blood thirster
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={deathsdance}
+                          alt="Deaths Dance Draven Survivor Build"
+                        />
+                        Deaths Dance
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={EdgeofNight}
+                          alt="Edge of Night-Draven Survivor Build"
+                        />
+                        Edge of Night
+                      </div>
+                      <div className="Draven-items">
+                        <img
+                          src={guardianangel}
+                          alt="Guardian Angel Draven Survivor Build"
+                        />
+                        Guardian Angel
+                      </div>
+                    </div>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
